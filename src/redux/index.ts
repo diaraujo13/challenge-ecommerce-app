c6113bf6bbc8d5f4195ad4ecd10517edc6ddeaf6
Izaías Araújo <izaiasousa@gmail.com>
2021-09-04 16:01:40 -0300

    feat: default export file

---
+++ src/redux/index.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+//   const store = configureStore({
+//     reducer,
+//     preloadedState,
+//     devTools: devtoolMiddleware,
+//   });
+
+  export default store;
