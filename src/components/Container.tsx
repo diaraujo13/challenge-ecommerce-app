3ce25a9a949977e566ca81fe5b6a974eaea17e9b
Izaías Araújo <izaiasousa@gmail.com>
2021-09-04 16:02:31 -0300

    feat: container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,26 @@
+import { KeyboardAvoidingView } from 'native-base';
+import React from 'react';
+import { Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import styled from 'styled-components/native';
+
+const ContainerWrapper = styled.View`
+  flex: 1;
+  padding: 10px;
+`;
+
+export default ({ children, style, ...props }) => (
+  <>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ContainerWrapper>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+          contentContainerStyle={{ flex: 1 }}
+        >
+          {children}
+        </KeyboardAvoidingView>
+      </ContainerWrapper>
+    </SafeAreaView>
+  </>
+);
