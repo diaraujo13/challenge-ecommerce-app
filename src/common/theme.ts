74d35c04d635253b2a05f038822bd9b288a22c7c
Izaías Araújo <izaiasousa@gmail.com>
2021-09-04 16:02:12 -0300

    feat: extend native base theme object and changed components base style

---
+++ src/common/theme.ts
@@ -0,0 +1,42 @@
+import { extendTheme } from 'native-base';
+
+const theme = extendTheme({
+  colors: {
+    // Add new color
+    primary: {
+      400: '#3fbffe',
+    },
+    // Redefinig only one shade, rest of the color will remain same.
+    amber: {
+      400: '#d97706',
+    },
+  },
+  config: {
+    // Changing initialColorMode to 'dark'
+    initialColorMode: 'light',
+  },
+  components: {
+    Button: {
+      baseStyle: {
+        marginVertical: 10,
+        borderRadius: 60,
+        height: '50px',
+        width: '70%',
+        alignSelf: 'center',
+      },
+      defaultProps: {},
+    },
+    Input: {
+      baseStyle: {
+        marginBottom: 5,
+        justifyContent: 'center',
+        paddingTop: 8,
+        borderRadius: 10,
+        height: 60,
+      },
+    },
+    Link: {},
+  },
+});
+
+export default theme;
