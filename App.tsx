2343039df47f7727d1847db6e5bf66b1b747e54b
Izaías Araújo <izaiasousa@gmail.com>
2021-09-04 16:03:58 -0300

    feat: apply theme object to Theme Provider

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider as ThemeProvider } from 'native-base';
+import { theme } from 'common';
+import { useCachedResources } from 'hooks';
+import Navigation from './src/routes';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
   }
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar />
+      <Navigation />
+    </ThemeProvider>
+  );
 }
