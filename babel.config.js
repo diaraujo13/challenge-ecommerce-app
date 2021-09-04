faa106f634dce1c04e7114c8eb796f75ab9c9b82
Izaías Araújo <izaiasousa@gmail.com>
2021-09-04 01:30:59 -0300

    chore: absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: ['react-native-reanimated/plugin', [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          components: './src/components',
+          screens: './src/screens',
+          redux: './src/redux',
+          hooks: './src/hooks',
+          config: './src/config',
+          assets: './src/assets',
+        },
+      }],
+    ],
   };
 };
