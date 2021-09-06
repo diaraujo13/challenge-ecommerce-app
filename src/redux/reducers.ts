a0a373b4b83295de3c9d8e76745f04480dc7fb58
Izaías Araújo <izaiasousa@gmail.com>
2021-09-06 01:59:10 -0300

    feat: added cart reducer

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import { reducer as productReducer } from '~/redux/modules/products/slices';
+import { reducer as cartReducer } from '~/redux/modules/cart/slices';
 
 export default combineReducers({
   products: productReducer,
+  cart: cartReducer,
 });
