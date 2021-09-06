903a28d9a40ebf87f2aacbbb471d57d99d0bc693
Izaías Araújo <izaiasousa@gmail.com>
2021-09-06 01:26:07 -0300

    feat: product list connected with redux

--- src/screens/Product/List/index.tsx
+++ src/screens/Product/List/index.tsx
@@ -1,62 +1,37 @@
-import React, { Component, useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import axios from 'axios';
-import { Box, FlatList } from 'native-base';
+import React, { useEffect, useState, useCallback } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, Box, FlatList, Heading } from 'native-base';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from 'components';
-import { apiService } from 'common';
+import * as selectors from '~/redux/selectors';
+import { ProductRow } from '~/components';
+import { productActions } from '~/redux/actions';
 
-interface IProduct {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: Record<string, any>;
-}
+const ProductList = props => {
+  const navigation = useNavigation();
 
-const ProductList = () => {
-  const [products, setProducts] = useState<Array<IProduct>>([]);
-
-  useEffect(() => {
-    (async function () {
-      try {
-        const { data } = await axios.get<IProduct[]>(
-          'https://fakestoreapi.com/products',
-        );
-        setProducts(data);
-      } catch (e) {
-        alert('Ocorreu um erro com sua requisição');
-      }
-    })();
-  }, []);
+  const products = useSelector(selectors.getAllProducts);
+  const dispatch = useDispatch();
 
   return (
     <>
-      <Container>
-        <FlatList
-          data={products}
-          renderItem={({ item }) => (
-            <Box px={5} py={2} rounded="md" my={2} bg="primary.300">
-              {item.title}
-            </Box>
-          )}
-          keyExtractor={item => item.id}
-        />
-      </Container>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <ProductRow
+            onPress={() => {
+              const { id } = item;
+              dispatch(productActions.selectProduct(id));
+              navigation.navigate('ProductDetails');
+            }}
+            {...item}
+          />
+        )}
+        keyExtractor={item => item.id}
+      />
     </>
   );
 };
 
-// define your styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2c3e50',
-  },
-});
-
-// make this component available to the app
 export default ProductList;
