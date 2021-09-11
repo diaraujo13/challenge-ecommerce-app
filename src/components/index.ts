c6a14f7ca4dfbfa67a3432c3386e5d06ad2ba62e
Izaías Araújo <izaiasousa@gmail.com>
2021-09-11 11:13:56 -0300

    feat: exported new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 export { default as SearchBar } from './form/SearchBar';
+export { default as FormInput } from './form/FormInput';
 
 export { default as Container } from './layout/Container';
 export { default as ProductRow } from './layout/ProductRow';
 export { default as ModalHeader } from './layout/ModalHeader';
 export { default as CartProductRow } from './layout/CartProductRow';
+export { default as DrawerContent } from './layout/DrawerContent';
 
 export { default as CartButton } from './widgets/CartButton';
 export { default as CloseButton } from './widgets/CloseButton';
